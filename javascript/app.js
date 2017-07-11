66e45287a9d382eae1f03caa7f0353c6e242d2d8
Frank87 <franklinsegura@Franklins-MacBook-Air.local>
2017-07-10 21:32:23 -0400

    files

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,8 @@
 //music to play when website loads.
-window.onload = function(){
+
 myAudio = new Audio('images/funmusic.mp3');
 myAudio.loop = true;
 myAudio.play();
-};
 
 //setting my array of movies
 var movies = ["Kill Bill", "King Kong", "John Wick", "Django Unchained","The Hateful Eight", "The Last Samurai", "Jack Reacher", "Home Alone 2", "Terminator 2: Judgment Day", "Hackers", "Snowden", "The Martian", "E.T. the Extra-Terrestrial", "Johnny 5", "Back to the Future", "Ferris Bueller's Day Off", "Raiders of the Lost Ark", "Return of the Jedi", "A Nightmare on Elm Street", "Footloose"];
